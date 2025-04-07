f0b52536e15a9d085d55827a5589d8b7db0ba9a6
Tyler Zon <tylerzon@yahoo.com>
2025-04-07 14:27:17 -0400

    Fixed page.tsx saying tailwind working

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 'use client';
+
 import MainDashboard from '../components/MainDashboard';
 
 export default function Page() {
   return (
     <div className="p-6">
-      <div className="bg-blue-600 text-white p-4 rounded-lg shadow text-xl">
-        ✅ Tailwind is working
-      </div>
       <MainDashboard />
     </div>
   );
